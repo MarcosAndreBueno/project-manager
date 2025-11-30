ab4f478d1edb35c988ecf2e18b5d626c9a59c59d
Marcos Bueno <www.bueno@yahoo.com>
2025-11-30 12:26:43 -0300

    Add to Kanban: initial layout

--- frontend/src/app/main/kanban/kanban.component.ts
+++ frontend/src/app/main/kanban/kanban.component.ts
@@ -1,12 +1,35 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { TicketStatusEnums } from '../../enums/ticket-status';
 
 @Component({
   selector: 'app-kanban',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './kanban.component.html',
   styleUrl: './kanban.component.scss'
 })
-export class KanbanComponent {
+export class KanbanComponent implements OnInit {
 
+  public statusList: TicketStatusEnums[] = [];
+  public statusEnums!: TicketStatusEnums[];
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    this.statusEnums = Object.values(TicketStatusEnums);
+  }
+
+  addColumn(status: TicketStatusEnums) {
+    if (!this.statusList.find(s => s == status)) {
+      this.statusList.push(status);
+    }
+    console.log('inside add', status);
+  }
+
+  removeColumn(status: TicketStatusEnums) {
+    this.statusList = this.statusList.filter(s => s !== status);
+    console.log('inside remove');
+  }
 }
