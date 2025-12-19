b7ece6eaa123d37ab986b417702bac53bd4b216a
Marcos Bueno <www.bueno@yahoo.com>
2025-12-19 12:31:04 -0300

    Upgrade to Angular 21

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -1,16 +1,12 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { CommonModule } from "@angular/common";
-
-
-
 
 @Component({
     selector: 'app-main',
     templateUrl: './main.component.html',
     styleUrls: ['./main.component.scss'],
     standalone: true,
-    imports: [CommonModule, RouterModule]
+    imports: [RouterModule]
 }) export class MainComponent {
 
     sidebar = true;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,14 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import  routeConfig from './app/app.routes';
 import { AppComponent } from './app/app.component';
 import { provideRouter } from '@angular/router';
-import { importProvidersFrom } from '@angular/core';
-import {HttpClientModule} from '@angular/common/http';
+import { provideHttpClient, withFetch} from '@angular/common/http';
+import { provideZoneChangeDetection } from '@angular/core';
 
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routeConfig),
-    importProvidersFrom(HttpClientModule),
+    provideZoneChangeDetection(),
+    provideHttpClient(withFetch()),
   ]
 }).catch((err) => console.error(err));
