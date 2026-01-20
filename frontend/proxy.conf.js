a775f4db064bbc6906d570177e0e28e2daf7a62e
Marcos Bueno <www.bueno@yahoo.com>
2026-01-20 15:52:47 -0300

    Add frontend to Docker and fix routes

--- frontend/proxy.conf.js
+++ frontend/proxy.conf.js
@@ -1,10 +1,22 @@
 const PROXY_CONFIG = [
     {
-      context: ['/api'],
-      target: 'http://localhost:8080/',
+      context: ['/api/tickets'],
+      target: 'http://ticket-service:8080/',
       secure: false,
       logLevel: 'debug'
-    }
+    },
+    {
+      context: ['/api/users'],
+      target: 'http://user-service:8081/',
+      secure: false,
+      logLevel: 'debug'
+    },
+    {
+      context: ['/api/teams'],
+      target: 'http://team-service:8082/',
+      secure: false,
+      logLevel: 'debug'
+    },
   ];
-  
+
   module.exports = PROXY_CONFIG;
--- user-service/src/main/java/com/projectmanager/user/controllers/UserConfigController.java
+++ user-service/src/main/java/com/projectmanager/user/controllers/UserConfigController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(value = "/api/user-config")
+@RequestMapping(value = "/api/users/config")
 public class UserConfigController {
 
     @Autowired
